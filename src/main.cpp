e1441c0880344c08fa65fef584e6b727a669b6f9
Aleksandr Volkov <ramwoolf@gmail.com>
2019-02-12 00:30:06 +0300

    Add version macroconstants from cmake

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,17 @@
+#ifndef MAJOR_VERSION_NUMBER
+#define MAJOR_VERSION_NUMBER 0
+#endif
+
+#ifndef MINOR_VERSION_NUMBER
+#define MINOR_VERSION_NUMBER 0
+#endif
+
+
 #include <iostream>
 
 int main(int argc, char const *argv[])
 {
-    std::cout << "Ololo" << std::endl;
+    std::cout << "Version" << std::endl;
+    std::cout << MAJOR_VERSION_NUMBER << "." << MINOR_VERSION_NUMBER << std::endl;
     return 0;
 }
